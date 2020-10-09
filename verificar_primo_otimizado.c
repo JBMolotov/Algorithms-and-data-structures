d0dab005c2545e241e3b5363f51cb832c5bdeff8
Johnny da Silva <45900493+JBMolotov@users.noreply.github.com>
2020-10-09 10:41:43 -0300

    Add files via upload

---
+++ verificar_primo_otimizado.c
@@ -0,0 +1,44 @@
+// Johnny Batista da Silva
+// 11371350
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int Recorrencia(int x, int y)
+{
+    int primo = 1; // a
+
+    if(y > 1) //caso base f(1) = 1
+    {
+        if ((x / y) * y == x) //c 
+        {
+            primo = 0;
+        }
+        else //pior caso
+        {
+            primo = Recorrencia(x, y-1); //a + f(x-1)
+        }
+    }
+    return primo;
+}
+
+int main(void)
+{
+    int n, x;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) // n (a + c)
+    {
+        scanf("%d", &x);
+        int p = Recorrencia(x, sqrt(x)-1); // n+a(f((raiz de x)-1))
+        if (p) 
+            printf("%d Primo\n", x);
+        else 
+            printf("%d Nao primo\n", x);
+    }
+
+    return 0;
+}
+
+//JBMolotov
